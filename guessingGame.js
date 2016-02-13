a85d9dab60733c33e759d0a1e6b46cc7c16954cb
Hannah <hannah.ko.wood@gmail.com>
2016-02-13 05:18:07 -0500

    Update guessingGame.js

--- guessingGame.js
+++ guessingGame.js
@@ -54,9 +54,10 @@ function lowerOrHigher(){
 
 function checkGuess(){
 	if (playersGuess === winningNumber){
-		$("div.message" ).html( "<p><b>You won!</b></p>");
 		var elt = document.getElementById("winMessage");
 		elt.setAttribute("style", "color: red", "font-size: 15pt");
+		$("div.message" ).html( "<p><b>You won!</b></p>");
+		
 		guesses = [];
 	}
 	else if (100 < playersGuess && playersGuess< 1 ) {
