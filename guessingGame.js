bb5f7edd6be2c55d175c9f8df4a16d61bc757729
Hannah <hannah.ko.wood@gmail.com>
2016-02-13 00:35:51 -0500

    add functions
    lowerOrHigher()
    checkGuess()

--- guessingGame.js
+++ guessingGame.js
@@ -20,21 +20,58 @@ function generateWinningNumber(){
 
 
 function playersGuessSubmission(){
-	var playersGuess = parseInt(document.getElementById("sumbitValue").value, 10);
+	playersGuess = +document.getElementById("sumbitValue");
 	document.getElementById("sumbitValue").value = '';
 }
 
 // Determine if the next guess should be a lower or higher number
 
 function lowerOrHigher(){
-	// add code here
+	var difference = Math.abs(winningNumber - playersGuess);
+	var hilo = '';
+	if (winningNumber < playersGuess) {
+		hilo = 'higher';
+	}
+	else (hilo = 'lower');
+	if (difference >= 20) {
+		return ("Your Guess is " + hilo + " and 20 or more digits away from the Winning Number!");
+	}
+	else if (10 <= difference < 20){
+		return ("Your Guess is " + hilo + " and less than 20 digits away from the Winning Number!");
+	}
+	else if (5 < difference < 10){
+		return ("Your Guess is " + hilo + " and less than 10 digits away from the Winning Number!");
+	}
+	else {
+		return ("Your Guess is " + hilo + " and within 5 digits of the Winning Number!");
+	}
 	
 }
 
 // Check if the Player's Guess is the winning number 
 
 function checkGuess(){
-	// add code here
+	var guesses = [];
+	if (playersGuess === winningNumber){
+		$("div.message" ).html( "<p><b>You won!</b></p>");
+		continue;
+	}
+	else if (100 < playersGuess < 1 ) {
+		$("div.message" ).html( "<p>That number is not between 1 and 100.</p>");
+		continue;
+	}
+	for (i = 0, i < guesses.length, i++) {
+		if (playersGuess === guesses[i]){
+			$("div.message" ).html( "<p>You already guessed that number.</p>");
+		}
+		else {
+			guesses.push(playersGuess);
+			guessesLeft = guesses.length;
+			$("div.message" ).html( "<p>Try again. </p>");
+			$("div.message" ).append( document.createTextNode(guessesLeft.toString() + " guesses left"));
+		}
+	}
+
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
